39ccc85ee4e43182129d9241db709b28b369e887
Tiago Lascasas dos Santos <up201503616@fe.up.pt>
2019-06-14 23:20:33 +0100

    Really fixed GitHub pages

---
+++ gh-pages.cmd
@@ -0,0 +1,11 @@
+setlocal
+copy index.html docs
+copy dist/bundle.js docs
+cd css
+copy style.css ../docs
+cd ..
+move style.css docs
+cd docs
+move style.css css
+move bundle.js dist/bundle.js 
+cd ..
